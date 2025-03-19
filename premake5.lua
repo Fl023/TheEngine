5753f88c611ee82cbf5ffc0f8e6b672bda081ab3
Fl023 <115723377+Fl023@users.noreply.github.com>
2025-03-19 14:01:17 +0100

    changed imgui.

--- premake5.lua
+++ premake5.lua
@@ -15,6 +15,7 @@ workspace "TheEngine"
  IncludeDir["GLFW"] = "TheEngine/vendor/GLFW/include"
  IncludeDir["Glad"] = "TheEngine/vendor/Glad/include"
  IncludeDir["ImGui"] = "TheEngine/vendor/imgui"
+ IncludeDir["glm"] = "TheEngine/vendor/glm"
 
  include "TheEngine/vendor/GLFW"
  include "TheEngine/vendor/Glad"
@@ -35,7 +36,9 @@ workspace "TheEngine"
  	files
  	{
  		"%{prj.name}/src/**.h",
- 		"%{prj.name}/src/**.cpp"
+ 		"%{prj.name}/src/**.cpp",
+        "%{prj.name}/vendor/glm/glm/**.hpp",
+        "%{prj.name}/vendor/glm/glm/**.inl"
  	}
  
  	includedirs
@@ -44,7 +47,8 @@ workspace "TheEngine"
  		"%{prj.name}/vendor/spdlog/include",
         "%{IncludeDir.GLFW}",
         "%{IncludeDir.Glad}",
-        "%{IncludeDir.ImGui}"
+        "%{IncludeDir.ImGui}",
+        "%{IncludeDir.glm}"
  	}
 
     links
@@ -107,7 +111,8 @@ workspace "TheEngine"
  	includedirs
  	{
  		"TheEngine/vendor/spdlog/include",
- 		"TheEngine/src"
+ 		"TheEngine/src",
+        "%{IncludeDir.glm}"
  	}
  
  	links
